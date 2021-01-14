73d8c7eefbce15f2b3afa76c96c71cd911d4bc8a
Thomas Sankara <thomassankara74@gmail.com>
2021-01-14 17:07:19 +0300

    audi homepage clone - complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Showcase from './components/Showcase'
+import AudiWorld from './components/AudiWorld'
+import LookingBack from './components/LookingBack'
+import Future from './components/Future'
+import Sustainability from './components/Sustainability'
+import Home from './components/Home'
+import Discover from './components/Discover'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Showcase />
+      <AudiWorld />
+      <LookingBack />
+      <Future />
+      <Sustainability />
+      <Home />
+      <Discover />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
